333c2b35c8e410230f22daa365ff379d49e3c2cf
claytonfortunato <claytonreis10@gmail.com>
2023-04-02 23:00:48 -0300

    feat: data api

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import * as C from "./style";
 
 const Main = () => {
---
+++ src/data/blocos.ts
@@ -0,0 +1,74 @@
+export const Blocos = [
+  {
+    title: "O Python do vovô não sobe mais",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_python.svg",
+    city: "São Paulo",
+    abbreviation: "SP",
+  },
+  {
+    title: "Todo mundo Null",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_mundo_null.svg",
+    city: "Florianópolis",
+    abbreviation: "SC",
+  },
+  {
+    title: "Hoje dou exception",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_exception.svg",
+    city: "Curitiba",
+    abbreviation: "PR",
+  },
+  {
+    title: "Manda Node",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_manda_node.svg",
+    city: "Salvador",
+    abbreviation: "BA",
+  },
+  {
+    title: "Só no back-end",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_back_end.svg",
+    city: "São Paulo",
+    abbreviation: "SP",
+  },
+  {
+    title: "Esse anel não é de Ruby",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_ruby.svg",
+    city: "São Paulo",
+    abbreviation: "SP",
+  },
+  {
+    title: "Pimenta no C# dos outros é refresco",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_C.svg",
+    city: "Rio de Janeiro",
+    abbreviation: "RJ",
+  },
+  {
+    title: "EnCACHE aqui",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_encache.svg",
+    city: "Porto Alegre",
+    abbreviation: "RS",
+  },
+  {
+    title: "Não valho nada mas JAVA li",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    image: "../src/assets/Img_blocos/bloco_java.svg",
+    city: "São Paulo",
+    abbreviation: "SP",
+  },
+];
