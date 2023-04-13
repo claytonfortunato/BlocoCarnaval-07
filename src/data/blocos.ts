1ad1f00a00648442fe25e0e29839c1f213abcbf9
claytonfortunato <claytonreis10@gmail.com>
2023-04-13 13:36:24 -0300

    feat: projeto finalizado and ajustes em deply

--- src/data/blocos.ts
+++ src/data/blocos.ts
@@ -3,7 +3,7 @@ export const Blocos = [
     title: "O Python do vovô não sobe mais",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-01.png",
+    image: "src/assets/img-bloco/bloco-01.png",
     city: "São Paulo",
     abbreviation: "SP",
   },
@@ -11,7 +11,7 @@ export const Blocos = [
     title: "Todo mundo Null",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-02.svg",
+    image: "src/assets/img-bloco/bloco-02.svg",
     city: "Florianópolis",
     abbreviation: "SC",
   },
@@ -19,7 +19,7 @@ export const Blocos = [
     title: "Hoje dou exception",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-03.svg",
+    image: "src/assets/img-bloco/bloco-03.svg",
     city: "Curitiba",
     abbreviation: "PR",
   },
@@ -27,7 +27,7 @@ export const Blocos = [
     title: "Manda Node",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-04.svg",
+    image: "src/assets/img-bloco/bloco-04.svg",
     city: "Salvador",
     abbreviation: "BA",
   },
@@ -35,7 +35,7 @@ export const Blocos = [
     title: "Só no back-end",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-05.svg",
+    image: "src/assets/img-bloco/bloco-05.svg",
     city: "São Paulo",
     abbreviation: "SP",
   },
@@ -43,7 +43,7 @@ export const Blocos = [
     title: "Esse anel não é de Ruby",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-06.svg",
+    image: "src/assets/img-bloco/bloco-06.svg",
     city: "São Paulo",
     abbreviation: "SP",
   },
@@ -51,7 +51,7 @@ export const Blocos = [
     title: "Pimenta no C# dos outros é refresco",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-07.svg",
+    image: "src/assets/img-bloco/bloco-07.svg",
     city: "Rio de Janeiro",
     abbreviation: "RJ",
   },
@@ -59,7 +59,7 @@ export const Blocos = [
     title: "EnCACHE aqui",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-08.svg",
+    image: "src/assets/img-bloco/bloco-08.svg",
     city: "Porto Alegre",
     abbreviation: "RS",
   },
@@ -67,7 +67,7 @@ export const Blocos = [
     title: "Não valho nada mas JAVA li",
     description:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-    image: "../src/assets/img-bloco/bloco-09.svg",
+    image: "src/assets/img-bloco/bloco-09.svg",
     city: "São Paulo",
     abbreviation: "SP",
   },
